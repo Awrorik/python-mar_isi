c7304c1969d7246b94761e257ce097b1bdcd756a
Мария <mariamalyh498@gmail.com>
2026-05-01 19:40:51 +0300

    added hello file

--- 1 cw.py
+++ 1 cw.py
@@ -1,1 +1,15 @@
-print ("Hello World!")
+
+import turtle
+
+t = turtle.Turtle()
+t.speed(5)
+
+t.fillcolor("pink")
+t.begin_fill()
+
+for i in range(4):
+     t.forward(100)
+     t.right(90)
+
+t.end_fill()
+turtle.done()
